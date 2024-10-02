57fda9bea7f20f8e91a67e18ec37e763b455aa38
namague00 <alfaiztaufiqurrahman3@gmail.com>
2024-10-02 16:05:33 +0700

    selesai

--- soal-2/Waktu.php
+++ soal-2/Waktu.php
@@ -2,9 +2,25 @@
 class Waktu
 {
     // deklarasikan atribut sesuai ditentukan buku
+    public $jam;
+    public $menit;
+    public $detik;
+
+    public function __construct($jam,$menit,$detik)
+    {
+        $this -> jam = $jam;
+        $this -> menit = $menit;
+        $this -> detik = $detik;
+    }
 
     // deklarasikan metode sesuai yang diminta buku
+    public function __get($atribut){
+        return $this -> $atribut;
+    }
+    public function __set($atribut,$nilai){
+        $this -> $atribut = $nilai;
+    }
 }
 
-$contoh = new Waktu(15,34,50);
-echo "Sekarang pukul".$contoh->jam.":".$contoh->menit.":".$contoh->detik."<br>"; //output: Sekarang pukul 15:34:50
+$contoh = new Waktu(15, 34, 50);
+echo "Sekarang pukul" . $contoh->jam . ":" . $contoh->menit . ":" . $contoh->detik . "<br>"; //output: Sekarang pukul 15:34:50
--- soal-3/Mobil.php
+++ soal-3/Mobil.php
@@ -1,15 +1,60 @@
 <?php
 class Mobil {
     // lengkapi dengan atribut dan metode sesuai pada buku
+    public $nopolisi;
+    public $merek;
+    public $type;
+    public $tahun;
+    public $pembuatan;
+    public $norangka;
+    public $nomesin;
+    public $bahanbakar;
+    public $bahanbaru;
 
     // contoh metode melaju()
+    public function getBahanbaru(){
+        return $this -> bahanbaru;
+    }
+    public function setBahanbaru($bahanbaru){
+        $this -> bahanbaru = $bahanbaru;
+    }
     public function melaju() {
         echo "Mobil sedang melaju.";
+        $this ->  bahanbakar -= 30;
     }
+    public function belok() {
+        echo "Mobil sedang belok.";
+        $this ->  bahanbakar -= 50;
+    }
+    public function berhenti() {
+        echo "Mobil sedang berhenti.";
+        $this ->  bahanbakar -= 20;
+    }
+    public function mundur() {
+        echo "Mobil sedang mundur.";
+        $this ->  bahanbakar -= 40;
+    }
+
     
 }
 
 // buat sebuah objek mobil dan lengkapi atributnya
+$ai = new mobil();
+$ai -> nopolisi = 123;
+$ai -> merek = "ami";
+$ai -> type = "berat";
+$ai -> tahun = 2003;
+$ai -> pembuatan = "papua";
+$ai -> norangka = 90;
+$ai -> nomesin = 80;
+$ai -> bahanbakar = 300;
+$ai -> bahanbaru = 300;
 
 
 // panggil keempat metode yang terdapat pada kelas Mobil
+echo "bahan bakar yang ada: ".$ai -> bahanbaru ."<br>";
+echo $ai -> melaju()."<br>";
+echo $ai -> belok()."<br>";
+echo $ai -> berhenti()."<br>";
+echo $ai -> mundur()."<br>";
+echo "sisa bahan bakar".$ai -> bahanbakar ."<br>";
