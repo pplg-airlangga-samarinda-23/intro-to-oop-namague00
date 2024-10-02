ccf2b7c2822015d77e43c6b9ce4c77199411aac9
namague00 <alfaiztaufiqurrahman3@gmail.com>
2024-10-02 14:16:36 +0700

    soal1

--- soal-1/Kotak.php
+++ soal-1/Kotak.php
@@ -2,25 +2,45 @@
 class PersegiPanjang
 {
     // deklarasikan panjang dan lebar
+    public $panjang;
+    public $lebar;
 
 
     // deklarasikan metode konstruktor
+    public function __construct($panjang,$lebar)
+    {
+        $this -> lebar = $lebar;
+        $this -> panjang = $panjang;
+    }
 
 
     // deklarasikan metode mutator dan asesor
+    public function getPanjang(){
+        return $this->panjang;
+    }
+    public function getLebar(){
+        return $this-> lebar;
+    }
+    public function setPanjang($panjang){
+        $this -> panjang = $panjang;
+    }
+    public function setLebar($lebar){
+        $this -> lebar = $lebar;
+    }
 
 
 
     public function hitungLuas()
     {
         // lengkapi kode untuk menghitung luas persegi panjang
+        return $this->panjang*$this ->lebar;
 
     }
 
     public function hitungKeliling()
     {
         // lengkapi dengan kode untuk menghitung keliling persegi panjang
-
+        return 2*($this -> panjang + $this -> lebar);
     }
 }
 
